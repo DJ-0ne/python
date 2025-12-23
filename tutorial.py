24000790b1597c755894dc8114f7fe70b4372668
Dj-0ne <davidjaoko1@gmail.com>
2025-12-23 13:23:04 +0300

    changes

--- tutorial.py
+++ tutorial.py
@@ -23,7 +23,11 @@ if (user_input == 2):
         result = print(f"Your choice is {options[choice]}  and Ai choice is {options[Ai]}")
         if(choice == 'r' and Ai == 's' or choice == 'p' and Ai == 'r' or choice == 's' and Ai == 'p'):
             print(f"you win {options[choice]}\nAi choice {options[Ai]}")
+        elif (choice == 'r' and Ai == 'r' or choice == 'p' and Ai == 'p' or choice == 's' and Ai == 's'):
+            print("It's a tie")
         else:
             print(f"you lose {options[choice]}\nAi choice {options[Ai]}")
+
+
         
         
